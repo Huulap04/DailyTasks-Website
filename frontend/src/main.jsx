031d238e21219106576bdaa48661378418094a85
Huulap04 <lapnh.ce190492@gmail.com>
2026-05-28 10:38:08 +0700

    update-Context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { AuthProvider } from './context/AuthContext.jsx';
+import { AuthProvider } from './Context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
--- frontend/src/page/Home/Home.jsx
+++ frontend/src/page/Home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../../api.js";
 import "./Home.css";
-import { useAuth } from "../../context/AuthContext.jsx";
+import { useAuth } from "../../Context/AuthContext.jsx";
 
 function Home() {
   const navigate = useNavigate();
